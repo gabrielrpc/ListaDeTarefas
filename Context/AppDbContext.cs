8dc0a3cbb868494b69fc425974ab1632614ff6a6
Gabriel <grpc15@outlook.com>
2024-10-04 12:18:57 -0300

    Adicionar arquivos de projeto.

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,31 @@
+﻿using ListaDeTarefas.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ListaDeTarefas.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Tarefa> Tarefas { get; set; }
+        public DbSet<Categoria> Caregoria { get; set; }
+        public DbSet<Status> Status { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Categoria>().HasData(
+                new Categoria { CategoriaId = "estudo", Nome = "Estudo" },
+                new Categoria { CategoriaId = "trabalho", Nome = "Trabalho" },
+                new Categoria { CategoriaId = "lazer", Nome = "Lazer" },
+                new Categoria { CategoriaId = "casa", Nome = "Casa" }
+            );
+
+            modelBuilder.Entity<Status>().HasData(
+                new Status { StatusId = "pendente", Nome = "Pendente" },
+                new Status { StatusId = "concluida", Nome = "Concluída" }
+            );
+        }
+    }
+}
---
+++ Models/Tarefa.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace ListaDeTarefas.Models
+{
+    public class Tarefa
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Necessário informar uma descrição!")]
+        public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "Informe a data de vencimento da tarefa!")]
+        public DateTime? DataVencimento { get; set; }
+
+        [Required(ErrorMessage = "Selecione uma categoria!")]
+        public string CategoriaId { get; set; }
+
+        [ValidateNever]
+        public Categoria Categoria { get; set; }
+
+        [Required(ErrorMessage = "Selecione um status!")]
+        public string StatusId { get; set; }
+
+        [ValidateNever]
+        public Status Status { get; set; }
+
+        public bool Atrasado => StatusId == "pendente" && DataVencimento < DateTime.Today;
+
+    }
+}
