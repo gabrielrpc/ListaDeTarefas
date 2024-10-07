12c06fa751be6fe779c7c7fc5829ff8b31784069
Gabriel <grpc15@outlook.com>
2024-10-07 09:04:07 -0300

    feat: cria acesso a view Adiconar, cria fétodos de deletar e adicionar

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using ListaDeTarefas.Context;
 using ListaDeTarefas.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -6,11 +7,120 @@ namespace ListaDeTarefas.Controllers
 {
     public class HomeController : Controller
     {
-
-        public IActionResult Index()
+        private readonly AppDbContext _context;
+        public HomeController(AppDbContext context)
         {
-            return View();
+            _context = context;
         }
 
+        public IActionResult Adicionar()
+        {
+            ViewBag.Categorias = _context.Caregoria.ToList();
+            ViewBag.Status = _context.Status.ToList();
+
+            var tarefa = new Tarefa { StatusId = "pendente" };
+
+            return View(tarefa);
+        }
+
+        public IActionResult Index(string filtroString)
+        {
+            var filtros = new Filtro(filtroString);
+
+            ViewBag.Filtros = filtros;
+            ViewBag.Categorias = _context.Caregoria.ToList();
+            ViewBag.Status = _context.Status.ToList();
+            ViewBag.Vencimento = Filtro.FiltroVencimentos;
+
+            IQueryable<Tarefa> consulta = _context.Tarefas;
+
+            if (filtros.TemCategoria)
+            {
+                consulta = consulta.Where(tarefa => tarefa.CategoriaId == filtros.CategoriaId);
+            }
+
+            if (filtros.TemStatus)
+            {
+                consulta = consulta.Where(tarefa => tarefa.StatusId == filtros.StatusId);
+            }
+
+            if (filtros.TemVencimento)
+            {
+                var hoje = DateTime.Today;
+
+                if(filtros.Passado)
+                {
+                    consulta = consulta.Where(tarefa => tarefa.DataVencimento < hoje);
+                }
+
+                if (filtros.Futuro)
+                {
+                    consulta = consulta.Where(tarefa => tarefa.DataVencimento > hoje);
+                }
+
+                if (filtros.Hoje)
+                {
+                    consulta = consulta.Where(tarefa => tarefa.DataVencimento == hoje);
+                }
+            }
+
+
+            var tarefas = consulta.OrderBy(tarefa => tarefa.DataVencimento).ToList();
+
+
+            return View(tarefas);
+        }
+
+        [HttpPost]
+        public IActionResult Filtrar(string[] filtro)
+        {
+            return RedirectToAction("Index", new { filtroString = string.Join("-", filtro) });
+        }
+
+        [HttpPost]
+        public IActionResult MarcarCompleto([FromRoute] string id, Tarefa tarefaSelecionada)
+        {
+            tarefaSelecionada = _context.Tarefas.Find(tarefaSelecionada.Id);
+
+            if (tarefaSelecionada != null)
+            {
+                tarefaSelecionada.StatusId = "concluido";
+                _context.SaveChanges();
+            }
+
+
+            return RedirectToAction("Index", new { filtroString = id });
+        }
+
+        [HttpPost]
+        public IActionResult Adicionar(Tarefa tarefa)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Tarefas.Add(tarefa);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Categorias = _context.Caregoria.ToList();
+            ViewBag.Status = _context.Status.ToList();
+
+            return View("Adicionar", tarefa);
+        }
+
+        [HttpPost]
+        public IActionResult DeletarCompletos(string id)
+        {
+            var paraDeletar = _context.Tarefas.Where(s => s.StatusId == "concluido").ToList();
+
+            foreach (var tarefa in paraDeletar)
+            {
+                _context.Tarefas.Remove(tarefa);
+            }
+                _context.SaveChanges();
+            return RedirectToAction("Index", new {ID = id});
+        }   
+               
     }
 }
---
+++ Models/Filtro.cs
@@ -0,0 +1,36 @@
+﻿namespace ListaDeTarefas.Models
+{
+    public class Filtro
+    {
+        public Filtro(string filtroString)
+        {
+            FiltroString = filtroString ?? "todos-todos-todos";
+            string[] filtros = FiltroString.Split("-");
+
+            CategoriaId = filtros[0];
+            Vencimento = filtros[1];
+            StatusId = filtros[2];
+        }
+
+        public string FiltroString { get; set; }
+        public string CategoriaId { get; set; }
+        public string StatusId { get; set; }
+        public string Vencimento { get; set; }
+
+
+        public bool TemCategoria => CategoriaId.ToLower() != "todos";
+        public bool TemVencimento => Vencimento.ToLower() != "todos";
+        public bool TemStatus => StatusId.ToLower() != "todos";
+
+        public static Dictionary<string, string> FiltroVencimentos => new Dictionary<string, string>
+        {
+            {"hoje", "Hoje"},
+            {"futuro", "Futuro"},
+            {"passado", "Passado"},
+        };
+
+        public bool Hoje => Vencimento.ToLower() == "hoje";
+        public bool Futuro => Vencimento.ToLower() == "futuro";
+        public bool Passado => Vencimento.ToLower() == "passado";
+    }
+}
